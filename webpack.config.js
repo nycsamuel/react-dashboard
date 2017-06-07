8131958196608e0ba7d89e9243b5d985cd86e9a2
nycsamuel <xsangmin@gmail.com>
2017-06-07 18:43:37 -0400

    add module

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,38 @@
-const path = require('path');
+'use strict'
 
-const config = {
+const path              = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack           = require('webpack');
+
+const APP_DIR   = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+
+module.exports = {
   entry: {
-    app: './src/components/App/App.js',
-    vendors: './src/vendors/vendor.js',
+    app: `${APP_DIR}/index.js`,
+    vendor: `${APP_DIR}/vendor/vendor.js`,
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  }
+    // filename: 'bundle.js', // for single entry point
+    filename: '[name].js', // for multiple entry points
+    path: BUILD_DIR,
+  },
+  module: {
+    rules: [
+      {test: /\.txt$/, use: 'raw-loader'},
+      {test: /\.css$/, use: [
+        {loader: 'style-loader'},
+        {loader: 'css-loader', options: {modules: true}},
+        // need style and css loader for css to be active
+      ]}, 
+   ],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'React Dashboard',
+      // template:'./src/index.html' // taken from webpack site
+      template: require('html-webpack-template'),
+    }),
+  ],
 };
-
-module.exports = config;
