7f03bad50476c672d31d2a7220367743cbc7236c
nycsamuel <xsangmin@gmail.com>
2017-06-08 14:02:49 -0400

    async method to load css on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 'use strict'
 
 const path              = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack           = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const APP_DIR   = path.resolve(__dirname, 'src');
 const BUILD_DIR = path.resolve(__dirname, 'dist');
@@ -10,26 +11,35 @@ const BUILD_DIR = path.resolve(__dirname, 'dist');
 module.exports = {
   entry: {
     app: `${APP_DIR}/index.js`,
-    vendor: `${APP_DIR}/vendor/vendor.js`,
+    // vendor: `${APP_DIR}/vendor/moment.js`,
+    vendor: 'moment',
   },
   output: {
     // filename: 'bundle.js', // for single entry point
-    filename: '[name].js', // for multiple entry points
+    // filename: '[name].js', // for multiple entry points
+    filename: '[name].[chunkhash].js', // for code splitting (vendor)
     path: BUILD_DIR,
   },
   module: {
     rules: [
       { test: /\.txt$/, use: 'raw-loader' },
-      // {test: /\.css$/, use: [
-      //   {loader: 'style-loader'},
-      //   {loader: 'css-loader', options: {modules: true}},
-      // ]}, 
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      // { test: /\.css$/, use: ['style-loader', 'css-loader'] }, // this is not asynchronous
+      { test: /\.css$/, use: ExtractTextPlugin.extract({ use: 'css-loader' }) }, // asynchronous method
    ],
   },
   plugins: [
+    new ExtractTextPlugin('style.css'), // enable async method to load css styles
     new webpack.HotModuleReplacementPlugin(), // enable HMR
     new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module) {
+        return module.context && module.context.indexOf('node_modules') !== -1; // assume vendor imports exists in the node_modules/
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest'
+    }),
     new HtmlWebpackPlugin({
       template:'./src/index.html' // taken from webpack site
       // template: require('html-webpack-template'), // from bobby's webpack
