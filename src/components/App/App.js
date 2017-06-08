c381d8c4dfacabb02ac975761a1c8464c8677f2f
nycsamuel <xsangmin@gmail.com>
2017-06-08 17:13:26 -0400

    react window resize listener and change state accordingly

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,13 +5,30 @@ import './normalize.css';
 export default class App extends Component {
   constructor() {
     super();
-    this.state = { test: "test" };
+    this.state = {
+      windowHeight: `${window.innerHeight}px`,
+      windowWidth: `${window.innerWidth}px`,
+    };
+    const wallpaper = document.getElementById('wallpaper');
+    this.handleWindowResize = this.handleWindowResize.bind(this);
+  }
+
+  componentDidMount() { 
+    window.addEventListener('resize', this.handleWindowResize);
+  }
+
+  handleWindowResize(event) {
+    this.setState({
+      windowHeight: `${window.innerHeight}px`,
+      windowWidth: `${window.innerWidth}px`, 
+    });
   }
 
   render() {
     return (
       <div className="container">
-        <h1>HELLO WORLD!</h1>
+        <div id="wallpaper"></div>
+        <h1>HELLO WORLD</h1>
       </div>
     );
   }
