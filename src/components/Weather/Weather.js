b96597e670419ee36ca7848801baae576a759db8
nycsamuel <xsangmin@gmail.com>
2017-06-10 19:04:02 -0400

    convert Kelvin to Fahrenheit

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -4,7 +4,10 @@ import style from './Weather.css';
 export default class Weather extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      name: '',
+      temp: '',
+    };
   }
 
   componentDidMount() {
@@ -17,15 +20,27 @@ export default class Weather extends Component {
       .then(res => res.json())
       .then(data => {
         console.log('weather data', data);
+        // this.setState({ currentWeather: data });
+        this.setState({ 
+          name: data.name,
+          temp: this.convertToFah(data.main.temp),
+        });
       })
       .catch(error => console.log('getWeather error', error));
 
     console.log('after fetch');
   }
 
+  convertToFah(k) {
+    return ((k * 9/5) - 459.67).toFixed(1);
+  }
+
   render() {
     return (
-      <div></div>
+      <div>
+        <h1>{this.state.name}</h1>
+        <p>{this.state.temp}<span className="fa fa-sun-o"></span></p>
+      </div>
     );
   }
 }
