f0776fd7f1fabc6b53649852c93dbeae4aba19bb
nycsamuel <xsangmin@gmail.com>
2017-06-13 15:18:23 -0400

    style quote component

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -5,7 +5,7 @@ import style from './Quote.css';
 const Quote = props => {
   return (
     <div className="quote-container">
-      <h3 className="q-text">{props.quote.quoteText}</h3>
+      <h1 className="q-text">{props.quote.quoteText}</h1>
     </div>
   );
 };
