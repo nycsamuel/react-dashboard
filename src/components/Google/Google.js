6e875e84a100404cd7ad90a2778af9750372a884
nycsamuel <xsangmin@gmail.com>
2017-06-21 16:00:26 -0400

    open new window/tab and search google

--- src/components/Google/Google.js
+++ src/components/Google/Google.js
@@ -4,7 +4,24 @@ import style from './Google.css';
 const Google = () => {
   return (
     <div className="google-container">
-      <input type="text" className="search-google" placeholder='What would you like to search?'/>
+      <input 
+        id="google-search-input"
+        type="text" 
+        className="search-google" 
+        placeholder='What would you like to search?'
+        onKeyPress={(event) => {
+          if (event.key === 'Enter') {
+            let search = document.getElementById('google-search-input');
+            if (search.value.trim() === '') return;
+
+            const googleURL = 'https://www.google.com/#q=';
+            let query = search.value.split(' ').join('+');
+            query = (query[query.length - 1] === '+') ? query.slice(0, -1) : query;
+            window.open(`${googleURL}${query}`, '_blank');
+            search.value = '';
+          }
+        }}
+      />
     </div>
   );
 }
