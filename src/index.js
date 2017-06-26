ace061c66f95eef391d52886ac53b3f26e0cfc91
nycsamuel <xsangmin@gmail.com>
2017-06-26 19:16:34 -0400

    remove react-router

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App.js';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Link
+// } from 'react-router-dom';
 
-ReactDOM.render(
-  <Router>
-    <Route path='/' component={App} />
-  </Router>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Router>
+//     <Route path='/' component={App} />
+//   </Router>,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(<App/>, document.getElementById('root'));
